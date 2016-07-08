69e0199343a4cd9eca4fa1bc8633444b5932f47b
ไกด์ <kittidechvongsak@kittidechs-macbook-air.local>
2016-07-08 16:57:00 +0700

    Initial Commit

---
+++ assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by Kittidech Vongsak on 7/8/16.
+//  Copyright © 2016 Kittidech Vongsak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
